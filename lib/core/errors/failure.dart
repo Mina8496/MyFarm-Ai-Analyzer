865526a257c04af129a78898602fbd28c1de5f8f
Mina Kamil <mena.m4077@gmail.com>
2026-04-13 16:55:36 +0200

    add class Failure

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,1 +1,55 @@
-class Failure {}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String message;
+  Failure(this.message);
+}
+
+class CacheFailure extends Failure {
+  CacheFailure(super.message);
+}
+
+class NetworkFailure extends Failure {
+  NetworkFailure(super.message);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.message);
+
+  factory ServerFailure.fromDiorError(DioError e) {
+    switch (e.type) {
+      case DioErrorType.connectionTimeout:
+        return ServerFailure('Connection timeout');
+      case DioErrorType.sendTimeout:
+        return ServerFailure('Send timeout');
+      case DioErrorType.receiveTimeout:
+        return ServerFailure('Receive timeout');
+      case DioErrorType.badResponse:
+        return ServerFailure.fromResponse(
+          e.response!.statusCode!,
+          e.response!.data,
+        );
+      case DioErrorType.cancel:
+        return ServerFailure('Request was cancelled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('NO Internet connection');
+
+      case DioErrorType.unknown:
+        return ServerFailure('Unexpected error: ${e.message}');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('bad certificate with Api Server');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 404) {
+      return ServerFailure('Not found');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal server error');
+    } else if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['message'] ?? 'Authentication error');
+    } else {
+      return ServerFailure('Received invalid status code: $statusCode');
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/get_navigation.dart';
