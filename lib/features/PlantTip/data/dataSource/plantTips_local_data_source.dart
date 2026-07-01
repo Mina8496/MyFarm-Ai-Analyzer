86687a7b459020eecb6f79f37fc0df34e528bc7d
Mina Kamil <mena.m4077@gmail.com>
2026-07-01 16:20:04 +0300

    refactor(PlantTipsLocalDataSource): This way, even if a sudden interruption occurs, the last stored copy will remain instead of being deleted first.
    - edit method : cachePlantTips in PlantTipsLocalDataSource

--- lib/features/PlantTip/data/dataSource/plantTips_local_data_source.dart
+++ lib/features/PlantTip/data/dataSource/plantTips_local_data_source.dart
@@ -13,7 +13,11 @@ class PlantTipsLocalDataSource {
   Box<PlantTipModel> get _box => Hive.box<PlantTipModel>(_boxName);
 
   Future<void> cachePlantTips(List<PlantTipModel> tips) async {
-    await _box.clear();
+    final newIds = tips.map((t) => t.id).toSet();
+    final oldKeys = _box.keys.where((k) => !newIds.contains(k)).toList();
+    if (oldKeys.isNotEmpty) {
+      await _box.deleteAll(oldKeys);
+    }
     await _box.putAll({for (var tip in tips) tip.id: tip});
   }
 
