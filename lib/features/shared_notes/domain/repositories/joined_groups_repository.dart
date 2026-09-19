976c532a1ad0d4a8a602837fd2c6aa893d2164b8
Mina Kamil <mena.m4077@gmail.com>
2026-09-19 18:22:14 +0300

    chore(shared_notes): remove dead joined-groups local-storage code
    - Removed JoinedGroupsLocalDataSource + JoinedGroupModel
      (SharedPreferences-based, fully replaced by Firestore's
      getMyGroups)
    - Removed JoinedGroupsRepository / JoinedGroupsRepositoryImpl
    - Removed GetJoinedGroupsUseCase
    - Removed their registrations from injection_container.dart
      (_setupSharedNotes)

--- lib/features/shared_notes/domain/repositories/joined_groups_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:myfarm/features/shared_notes/domain/entities/joined_group_entity.dart';
-
-abstract class JoinedGroupsRepository {
-  Future<List<JoinedGroupEntity>> getAll();
-  Future<void> upsert(JoinedGroupEntity group);
-  Future<void> remove(String id);
-}
--- lib/features/shared_notes/domain/usecases/get_joined_groups_usecase.dart
+++
@@ -1,9 +0,0 @@
-import 'package:myfarm/features/shared_notes/domain/entities/joined_group_entity.dart';
-import 'package:myfarm/features/shared_notes/domain/repositories/joined_groups_repository.dart';
-
-class GetJoinedGroupsUseCase {
-  final JoinedGroupsRepository repo;
-  GetJoinedGroupsUseCase(this.repo);
-  Future<List<JoinedGroupEntity>> call() => repo.getAll();
-}
-
